d7bf62e65c09eaaeef86c12350e05acac53f014a
Amlal El Mahrouss <amlal@nekernel.org>
2026-06-10 22:53:57 +0200

    [CHORE] src: adding proofs, glossarium patches.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ on_proofs/content/InductionSample.lean
@@ -0,0 +1,7 @@
+-- File: InductionSample.lean
+-- Author: Amlal El Mahrouss
+-- Purpose: Generate HTML and PDF papers from LaTex documents.
+-- (C) 2026 Amlal El Mahrouss.
+
+variable {p : Prop}
+variable {q : Prop}
